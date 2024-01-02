d91a9fcca4234d728cedd032ec39a0ffc56989ae
fritz <franciscosantin2002@gmail.com>
2024-01-02 17:32:26 -0300

    interacción con las notificaciones

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -52,7 +52,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     print('$token');
   }
 
-  void _handleRemoteMessage(RemoteMessage message) {
+  void handleRemoteMessage(RemoteMessage message) {
     if (message.notification == null) return;
     final notification = PushMessage(
         messageId:
@@ -68,7 +68,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   void _onForegroundMessage() {
-    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
+    FirebaseMessaging.onMessage.listen(handleRemoteMessage);
   }
 
   void requestPermissions() async {
